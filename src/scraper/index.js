28e485a2e7c3c2c7b9fe17381c0079c0ae91bca1
Ranjan Shrestha <ranjan@sharingapples.com>
2017-11-06 22:48:17 +0545

    Export library methods

---
+++ src/scraper/index.js
@@ -0,0 +1,15 @@
+import getDistricts from './getDistricts';
+import getLocalBodies from './getLocalBodies';
+import getWards from './getWards';
+import getCenters from './getCenters';
+import getVoterList from './getVoterList';
+import getVoterInfo from './getVoterInfo';
+
+export {
+  getDistricts,
+  getLocalBodies,
+  getWards,
+  getCenters,
+  getVoterList,
+  getVoterInfo,
+};
